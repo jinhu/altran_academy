5d27495d7258ccf1b70782185d15a58cad98a701
jin <jinmin.hu@altran.com>
2016-09-06 23:52:26 +0200

    make exec

--- 9_embedded_device/1_embedded_linux/cyber-dojo.sh
+++ 9_embedded_device/1_embedded_linux/cyber-dojo.sh
@@ -2,3 +2,4 @@ dmcs -t:library -r:/usr/lib/cli/nunit.framework-2.6/nunit.framework.dll -out:Run
 if [ $? -eq 0 ]; then
   nunit-console -nologo RunTests.dll
 fi
+#
--- 9_embedded_device/5_power_management/cyber-dojo.sh
+++ 9_embedded_device/5_power_management/cyber-dojo.sh
@@ -1,1 +1,2 @@
 python -m unittest *test*.py
+#
